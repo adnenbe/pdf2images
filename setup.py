07602db3c82480533f9aa928e994278280b884fe
adnenbe <adnen.bennour@hotmail.fr>
2019-05-25 12:06:45 +0100

    Add requirements and setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pdf2images',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='adnenbe',
+    author_email='adnen.bennour@hotmail.fr',
+    description='pdf2images is a script to convert one PDF to images'
+)
